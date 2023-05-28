a309c0be5591ba3aee84d046b2d675ab30119b58
ddsgar <92583082+ddsgar@users.noreply.github.com>
2023-05-29 00:34:22 +0300

    Create Prog_21.cpp

---
+++ Prog_21.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+template<typename T>
+void swaps(T& a, T& b) {
+    T temp = a;
+    a = b;
+    b = temp;
+}
+
+int main() {
+    int intA = 10, intB = 20;
+    std::cout << "Before swap: intA = " << intA << ", intB = " << intB << std::endl;
+    swaps(intA, intB);
+    std::cout << "After swap: intA = " << intA << ", intB = " << intB << std::endl;
+
+    char charA = 'A', charB = 'B';
+    std::cout << "Before swap: charA = " << charA << ", charB = " << charB << std::endl;
+    swaps(charA, charB);
+    std::cout << "After swap: charA = " << charA << ", charB = " << charB << std::endl;
+
+    float floatA = 3.14, floatB = 2.71;
+    std::cout << "Before swap: floatA = " << floatA << ", floatB = " << floatB << std::endl;
+    swaps(floatA, floatB);
+    std::cout << "After swap: floatA = " << floatA << ", floatB = " << floatB << std::endl;
+
+    return 0;
+}
