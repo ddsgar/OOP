f00cf018e972fae411b324e00fa55bd6125adf44
ddsgar <92583082+ddsgar@users.noreply.github.com>
2023-05-29 00:35:08 +0300

    Create Prog_22.cpp

---
+++ Prog_22.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+template<typename T>
+T amax(T* arr, int size) {
+    T max = arr[0];
+    for (int i = 1; i < size; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    return max;
+}
+
+int main() {
+    int intArr[] = {5, 2, 8, 1, 9};
+    int intSize = sizeof(intArr) / sizeof(int);
+    std::cout << "Max value in intArr: " << amax(intArr, intSize) << std::endl;
+
+    double doubleArr[] = {3.14, 1.23, 2.78, 0.99};
+    int doubleSize = sizeof(doubleArr) / sizeof(double);
+    std::cout << "Max value in doubleArr: " << amax(doubleArr, doubleSize) << std::endl;
+
+    char charArr[] = {'A', 'C', 'B', 'E'};
+    int charSize = sizeof(charArr) / sizeof(char);
+    std::cout << "Max value in charArr: " << amax(charArr, charSize) << std::endl;
+
+    return 0;
+}
