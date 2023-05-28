903c81932924a158fc71152adddc5315119e3b5e
ddsgar <92583082+ddsgar@users.noreply.github.com>
2023-05-28 23:57:32 +0300

    Create Prog_3.cpp

---
+++ Prog_3.cpp
@@ -0,0 +1,49 @@
+// Класс для комплексного числа
+class ComplexNumber {
+private:
+    double real;    // Действительная часть
+    double imaginary;   // Мнимая часть
+
+public:
+    // Конструктор
+    ComplexNumber(double r, double i) : real(r), imaginary(i) {}
+
+    // Методы доступа (геттеры) для чтения значений частей комплексного числа
+    double getReal() const {
+        return real;
+    }
+
+    double getImaginary() const {
+        return imaginary;
+    }
+
+    // Методы для выполнения операций над комплексными числами
+    ComplexNumber add(const ComplexNumber& other) const {
+        double newReal = real + other.real;
+        double newImaginary = imaginary + other.imaginary;
+        return ComplexNumber(newReal, newImaginary);
+    }
+
+    ComplexNumber subtract(const ComplexNumber& other) const {
+        double newReal = real - other.real;
+        double newImaginary = imaginary - other.imaginary;
+        return ComplexNumber(newReal, newImaginary);
+    }
+
+    ComplexNumber multiply(const ComplexNumber& other) const {
+        double newReal = (real * other.real) - (imaginary * other.imaginary);
+        double newImaginary = (real * other.imaginary) + (imaginary * other.real);
+        return ComplexNumber(newReal, newImaginary);
+    }
+};
+
+int main() {
+    ComplexNumber c1(2.0, 3.0);
+    ComplexNumber c2(1.0, 4.0);
+
+    ComplexNumber sum = c1.add(c2);
+    ComplexNumber difference = c1.subtract(c2);
+    ComplexNumber product = c1.multiply(c2);
+
+    cout << "Sum: " << sum.getReal() << " + " << sum.getImaginary() << "i" << endl;
+    cout << "Difference: " << difference.getReal() <<
